929d4613c7db70896bc68b6908a8b6cb4c383761
deftsoft786 <jatin.infotech88@gmail.com>
2018-05-10 16:59:49 +0530

    Add files via upload
    new about page

---
+++ about.php
@@ -0,0 +1,104 @@
+<?php
+require_once('header.php');
+?>
+<section class="about-page" >
+ <h1 class="text-center wyt page-title"> About Us </h1>
+</section>
+
+
+<section class="about1">
+  <div class="container">
+    <div class="about_bg">
+    <div class="row">
+
+<div class="col-md-4 col-sm-4 col-xs-12 mt-20">
+  
+<img src="img/about1.png" width="500" class="img-responsive img-rounded">
+
+</div>
+<div class="col-md-8 col-sm-8 col-xs-12  mt-20">
+
+
+<h2 class="text-left"> About Us  </h2>
+
+<p style="margin-top: 20px;">  
+ AK Designs and Creativatorss, a well-established firm of modelers who share a strong vision effortlessly recognized in their outline. 
+AK Designs and Creativatorss is likewise centered around utilizing the most current PC innovation in its plan procedure, both to comprehend the design and to communicate the outline to our customers. To this end, we are world pioneers in the utilization of Revit and have additionally created virtual reality apparatuses for use in our procedure. 
+This, combined with both an imaginative and devoted way to deal with the design, documentation, and execution of projects universally, has seen AK Design and Creativatorss turn into a globally looked for after brand.
+</p>
+
+  </div>
+
+
+    </div>
+     </div> 
+  </div>
+
+
+<div class="demo1 ">
+    <div class="container pb-20">
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <div class="serviceBox1" style="height: 404px;">
+                    <i class="service-icon fa fa-globe"></i>
+                    <h3 class="title"> Our Mission</h3>
+                    <p class="description">
+                       Committed to feed this growing appetite for design, AK Designs and Creativatorss has never hesitated in accepting projects in various verticals, as long as there is a design expertise involved at the core.<br>
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="serviceBox1" >
+                    <i class="service-icon fa fa-rocket"></i>
+                    <h3 class="title">Our Vision</h3>
+                    <p class="description">
+                        AK Designs and Creativatorss well-trained design team undertakes professional designing job & execute them in close co-ordination with various agencies engaged for the job. They have a well-equipped studio with complete facilities for end-to-end design solutions.
+                    </p>
+                </div>
+            </div>
+
+<div class="col-md-4 col-sm-6">
+                <div class="serviceBox1">
+                    <i class="service-icon fa fa-rocket"></i>
+                    <h3 class="title">Our Stratgy </h3>
+                    <p class="description">
+                        The modern / contemporary interior designs are matched with procurement of various high quality materials sourced from trusted suppliers in the country and abroad. The designs help create vibrant atmosphere conducive for better living.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+
+
+</section>
+
+
+
+
+
+
+
+<section class="client-logo">
+<div class="container">
+
+  <h2 class="text-center"> Our Clients </h2>
+ 
+  <ul class="text-center">
+    <li> <img src="img/logo1.png"></li>
+       <li> <img src="img/logo1.png"></li>
+           <li> <img src="img/logo1.png"></li>
+               <li> <img src="img/logo1.png"></li>
+                </ul>
+  
+
+</div>
+
+</section>
+
+<?php
+require_once('footer.php');
+?>
